46797503fd926f22581c414df61f271b12e38b47
NesterenkoIV <nesterenko@intervolga.ru>
2020-08-23 22:06:42 +0400

    Подключил новую реализацию контейнера для DI (cli-config)

--- public/config/cli-config.php
+++ public/config/cli-config.php
@@ -1,7 +1,9 @@
 <?php
+require_once 'prolog.php';
 
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use App\AppContainer;
 
-require_once 'prolog.php';
+$entityManager = AppContainer::getEntityManager();
 
 return ConsoleRunner::createHelperSet($entityManager);
